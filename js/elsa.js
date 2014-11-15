72337e3cc770b46c51f9303870006f37daa7a03e
mohlerm <marcel.mohler@gmail.com>
2014-11-15 14:35:56 +0100

    Added private message functionality

--- js/elsa.js
+++ js/elsa.js
@@ -20,16 +20,17 @@ var args = process.argv.slice(2);
 var path, retrans, difs, cwmin, cwmax;
 
 var argLength = args.length;
-if(argLength > 1) {
-    if(argLength > 1 && args[0]=='-p') {
+if (argLength > 1) {
+    if (argLength == 2 && args[0] == '-p' || argLength == 7 && args[2] == '-c') {
         path = args[1];
-    }
-    if (argLength > 6 && args[2]=='-c') {
-        retrans = args[3];
-        difs = args[4];
-        cwmin = args[5];
-        cwmax = args[6];
-        console.log("Using custom values path=%s, retrans=%s, difs=%s, cwmin=%s, cwmax=%s".green,path,retrans,difs,cwmin,cwmax);
+        console.log("Using custom path=%s".green, path);
+        if (argLength > 6 && args[2] == '-c') {
+            retrans = args[3];
+            difs = args[4];
+            cwmin = args[5];
+            cwmax = args[6];
+            console.log("Using custom values retrans=%s, difs=%s, cwmin=%s, cwmax=%s".green, retrans, difs, cwmin, cwmax);
+        }
     } else {
         console.log("Do not understand command line input; Please use: -p {PATH} -c {RETRANS} {DIFS} {CWMIN} {CWMAX} or leave empty for default".red);
         process.exit(1);
@@ -37,57 +38,96 @@ if(argLength > 1) {
 
 } else {
 // default values
-    if(isWin) {
+    if (isWin) {
         path = "COM4";
     } else {
-        path = "/dev/ttyACM1"
+        path = "/dev/ttyACM0"
     }
     retrans = 3;
     difs = 10;
     cwmin = 4;
     cwmax = 16;
-    console.log("Using default values path=%s, retrans=3, difs=10, cwmin=4, cwmax=16".green,path);
+    console.log("Using default values path=%s, retrans=3, difs=10, cwmin=4, cwmax=16".green, path);
 }
 var device = pserial.getDevice(path)
 
 device.connect()
-        .then(function(){console.log("Connection successful\nchat> ".green)},function(){console.log("Connection failed\nchat> ".green)})
-        .then(function(){return device.configure(retrans, difs, cwmin, cwmax)})
-        .then(function(){console.log("Configuration successful\nchat> ".green)},function(){console.log("Configuration failed\nchat> ".green)})
-        .then(function(){return device.enableCom()})
+    .then(function () {
+        console.log("Connection successful\nchat> ".green)
+    }, function () {
+        console.log("Connection failed\nchat> ".green)
+    })
+    .then(function () {
+        return device.configure(retrans, difs, cwmin, cwmax)
+    })
+    .then(function () {
+        console.log("Configuration successful\nchat> ".green)
+    }, function () {
+        console.log("Configuration failed\nchat> ".green)
+    })
+    //.then(function () { // it seems we don't need it since 'e' is default
+    //    return device.enableCom()
+    //})
 
-device.on('message', function(payload, statistics) {
-    console.log('echo> '+ payload.green);
+device.on('message', function (payload, statistics) {
+    console.log('echo> ' + payload.green);
 });
 
 
 var rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+    input: process.stdin,
+    output: process.stdout
 });
-rl.setPrompt('chat> ');
+rl.setPrompt('chat> '.green);
 rl.prompt();
 
 rl.on('line', function (msg) {
-  switch(msg) {
-    case 'quit':
-      console.log('Quitting...'.green);
-      rl.close();
-      process.exit(0);
-      break;
-    case 'ping':
-      console.log('echo> Pong!'.rainbow)
-      break;
-    case 'list':
-      pserial.list(function(devices){devices.forEach(function(device){console.log((device.path + '\n').green)})})
-    default:
-      device.send('FF',msg);
-      //console.log('echo> '+msg.green);
-  }
-  rl.prompt();
-}).on('close', function() {
-  process.exit(0);
+    switch (msg) {
+        case '/quit':
+            console.log('Quitting...'.green);
+            rl.close();
+            device.close();
+            process.exit(0);
+            break;
+        case '/ping':
+            console.log('echo> Pong!'.rainbow)
+            break;
+        case '/list':
+            pserial.list(function (devices) {
+                devices.forEach(function (device) {
+                    console.log((device.path + '\n').green)
+                })
+            })
+            break;
+        case '/spam':
+            console.log('generating traffic...'.red)
+            break;
+        default:
+            if(isPrivateMessage(msg)) {
+                device.send(msg.slice(1,3),msg.slice(4))
+            } else {
+                device.send('FF', msg);
+            }
+        //console.log('echo> '+msg.green);
+    }
+    rl.prompt();
+}).on('close', function () {
+    device.close();
+    process.exit(0);
 });
 
 
-//console.log('Hello World');
+function isPrivateMessage(msg) {
+    return hasPrefix(msg,'@') && isValidAddress(msg.slice(1,3))
+}
+
+function isValidAddress(addr) {
+    return addr.length == 2 // TODO: add check for valid hex number
+}
+
+function hasMessagePrefix(msg) {
+    return hasPrefix(msg,'@');
+}
+function hasPrefix(msg,char) {
+    return msg.length > 0 && msg[0] == char;
+}
